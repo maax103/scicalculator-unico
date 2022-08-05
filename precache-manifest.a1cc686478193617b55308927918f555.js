085b00b93de77d64599f006ec7cbee7a63a2a5dc
maax103 <maximilianomfurtado@gmail.com>
2022-08-05 17:35:15 -0300

    Updates

---
+++ precache-manifest.a1cc686478193617b55308927918f555.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "ffa74dabf9e95c19647e",
+    "url": "/scicalculator-unico/static/css/main.bdb1f8bc.chunk.css"
+  },
+  {
+    "revision": "ffa74dabf9e95c19647e",
+    "url": "/scicalculator-unico/static/js/main.ffa74dab.chunk.js"
+  },
+  {
+    "revision": "18bbc14cade66e61224a",
+    "url": "/scicalculator-unico/static/js/1.18bbc14c.chunk.js"
+  },
+  {
+    "revision": "98baa95a7a67b885c617",
+    "url": "/scicalculator-unico/static/js/runtime~main.98baa95a.js"
+  },
+  {
+    "revision": "c47a5a030fe93311acce2ece6f52e62c",
+    "url": "/scicalculator-unico/static/media/logo.c47a5a03.png"
+  },
+  {
+    "revision": "ea8b2c81a473dd841f806f48227fd70e",
+    "url": "/scicalculator-unico/static/media/refresh_icon.ea8b2c81.svg"
+  },
+  {
+    "revision": "4d9205a5605ecfdd37b7df3ad2805cd2",
+    "url": "/scicalculator-unico/index.html"
+  }
+];
